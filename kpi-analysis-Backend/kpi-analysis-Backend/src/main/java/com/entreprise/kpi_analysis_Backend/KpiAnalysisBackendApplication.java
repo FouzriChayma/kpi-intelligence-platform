cfb0e02cc4d198c04b3905bfd9d4bed041284073
FouzriChayma <chayma.fouzri@esprit.tn>
2025-11-17 08:49:42 +0100

    setup

---
+++ kpi-analysis-Backend/kpi-analysis-Backend/src/main/java/com/entreprise/kpi_analysis_Backend/KpiAnalysisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.entreprise.kpi_analysis_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiAnalysisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpiAnalysisBackendApplication.class, args);
+	}
+
+}
---
+++ kpi-analysis-Backend/kpi-analysis-Backend/src/test/java/com/entreprise/kpi_analysis_Backend/KpiAnalysisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entreprise.kpi_analysis_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpiAnalysisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
